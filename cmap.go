7ef93bc1dea62e51fc38e9fe6b7f78ac7b85aafe
James Bomysoad <jbomyso1.binghamton.edu>
2017-02-10 20:00:14 -0500

    Started cmap

---
+++ cmap.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "bytes"
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "math/rand"
+    "runtime"
+    "strings"
+    "sync"
+    "sync/atomic"
+    "time"
+)
+
+const {
+    ADD_BUFFER_SIZE = 32
+    ASK_BUFFER_SIZE = 8
+}
+
+func main() {
+
+}
